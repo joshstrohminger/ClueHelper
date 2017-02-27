1a0778b3b4972f2a4fcc3bcb5e3b6a827ea67827
Josh Strohminger <jdst8-git@yahoo.com>
2017-02-26 23:29:17 -0800

    Create initial simulator that can't do anything yet.

--- ScoreCardTest/Simulator.cs
+++ ScoreCardTest/Simulator.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScoreCard;
+using ScoreCard.Models;
 
 namespace ScoreCardTest
 {
     public class Simulator
     {
+        private Solver _solver;
+        public Card[] Solution { get; }
+        public Dictionary<Player,Card[]> PlayerHands { get; }
+
+        public Simulator()
+        {
+            var game = Config.BuildDefaultGame();
+            var me = game.Players.Last();
+            _solver = new Solver(game, me);
+
+            var random = new Random();
+            var shuffledCards = game.Categories
+                .SelectMany(category => category.Cards)
+                .OrderBy(card => random.Next())
+                .ToList();
+
+            Solution = shuffledCards.Take(game.CardsPerSuggestion).ToArray();
+            for (var i = 0; i < game.CardsPerSuggestion; i++)
+            {
+                shuffledCards.RemoveAt(0);
+            }
+
+            var numPlayers = game.Players.Count;
+            PlayerHands = game.Players.ToDictionary(p => p, (player) =>
+            {
+                var index = game.Players.IndexOf(player);
+                return shuffledCards.Where((c, i) => i % numPlayers == index).ToArray();
+            });
+
+            foreach (var card in PlayerHands[me])
+            {
+                _solver.PlayerHasCard(me, card, "Card was dealt.");
+            }
+        }
     }
 }
